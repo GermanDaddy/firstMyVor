082a570e157d1386fbf83edc0bfc1d81fe03a33c
Artem <guglyad@yandex.ru>
2019-01-19 13:57:55 +0200

    Added new log

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
 	public static void main(String[] args){
 		System.out.println("Hi, my friend");
 		System.out.println("Hello, my friend");
+		System.out.println("I ganna say the n word ");
 	}
 }
